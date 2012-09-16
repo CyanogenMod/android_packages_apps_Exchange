8f097d1c9b4a646d78546a119b0b210a31f47c5a
Marc Blank <mblank@google.com>
2012-09-15 18:12:59 -0700

    Fix directory provider authority name & export provider
    Bug: 7171739
    Change-Id: I15b0d386dda54584aaba86ce3b70c3588528515b

--- build/src/com/android/exchange/Configuration.java
+++ build/src/com/android/exchange/Configuration.java
@@ -20,6 +20,6 @@ public class Configuration {
     public static final String EXCHANGE_ACCOUNT_MANAGER_TYPE = "com.android.exchange";
     public static final String EXCHANGE_SERVICE_INTENT_ACTION =
             "com.android.email.EXCHANGE_INTENT";
-    public static final String EXCHANGE_GAL_AUTHORITY = "com.android.exchange.provider";
+    public static final String EXCHANGE_GAL_AUTHORITY = "com.android.exchange.directory.provider";
     public static final String EXCHANGE_PROTOCOL = "eas";
 }
